956b93508321c968cbb66bd6ddc8dd0368fb002f
jimin <jiminseo0307@gmail.com>
2021-06-28 00:49:28 +0900

    Update typechain

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,35 @@
 //   age = 24,
 //   gender = "female";
 
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age} and ${gender}!`;
+// interface Human {
+//     name: string;
+//     age: number;
+//     gender: string;
+// }
+
+// const person = {
+//     name: "jjack",
+//     age: 25,
+//     gender: "Female"
+// };
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+const jjackii = new Human("jjackii", 25, "Female");
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age} and ${person.gender}!`;
 };  
 
-console.log(sayHi("jjack", 25, "Female"));
+console.log(sayHi(jjackii))
 
 export {};
